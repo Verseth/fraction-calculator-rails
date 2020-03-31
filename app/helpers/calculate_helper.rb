9251cb5d4e01d60e79533c973fd8514501f55cac
Mateusz Drewniak <matmg24@gmail.com>
2020-04-01 01:06:45 +0200

    In app readme finished

--- app/helpers/calculate_helper.rb
+++ app/helpers/calculate_helper.rb
@@ -1,2 +1,13 @@
 module CalculateHelper
+  def special(string)
+    capture_haml {
+      haml_tag 'span.special-text', string
+    }.strip!.html_safe
+  end
+
+  def bold(string)
+    capture_haml {
+      haml_tag 'span.bold', string
+    }.strip!.html_safe
+  end
 end
